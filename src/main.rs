6c550f840edddeb57a789ec3cc6035b542b8b598
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-09-27 19:03:06 -0700

    add simulator test

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use dsim::message_bus::{Message, MessageBus, Subscriber};
+    use dsim::message_bus::{Message, MessageBus, Simulator, Subscriber};
     use std::{collections::VecDeque, time};
 
     /// PingPong will emit a ping every tick, and respond with a pong.
@@ -93,5 +93,67 @@ mod tests {
     }
 
     #[test]
-    fn test_simulator() {}
+    fn test_simulator() {
+        let ping_pong_1 = PingPong::new(
+            std::time::Duration::from_millis(1000),
+            "ping_pong_2",
+            "ping_pong_1",
+        );
+        let ping_pong_2 = PingPong::new(
+            std::time::Duration::from_millis(1000),
+            "ping_pong_1",
+            "ping_pong_2",
+        );
+        let simulator = Simulator::new(
+            maplit::hashmap! {
+                "ping_pong_1".to_string() => Box::new(ping_pong_1) as Box<dyn Subscriber>,
+                "ping_pong_2".to_string() => Box::new(ping_pong_2) as Box<dyn Subscriber>,
+            },
+            {
+                let start = std::time::Instant::now();
+                vec![
+                    // initial tick at start
+                    dsim::message_bus::SimulatorEvent::Tick(start),
+                    // deliver a Ping to ping_pong_1 shortly after start
+                    dsim::message_bus::SimulatorEvent::Envelope(
+                        dsim::message_bus::Envelope {
+                            message: Box::new(Ping {}),
+                            destination: "ping_pong_1".to_string(),
+                        },
+                        start + std::time::Duration::from_millis(100),
+                    ),
+                    // tick after 1.1s so ping_pong_1 can process held ping
+                    dsim::message_bus::SimulatorEvent::Tick(
+                        start + std::time::Duration::from_millis(1100),
+                    ),
+                    // deliver a Pong to ping_pong_2
+                    dsim::message_bus::SimulatorEvent::Envelope(
+                        dsim::message_bus::Envelope {
+                            message: Box::new(Pong {}),
+                            destination: "ping_pong_2".to_string(),
+                        },
+                        start + std::time::Duration::from_millis(1200),
+                    ),
+                    // another tick later
+                    dsim::message_bus::SimulatorEvent::Tick(
+                        start + std::time::Duration::from_millis(2000),
+                    ),
+                    // deliver another Ping to ping_pong_2
+                    dsim::message_bus::SimulatorEvent::Envelope(
+                        dsim::message_bus::Envelope {
+                            message: Box::new(Ping {}),
+                            destination: "ping_pong_2".to_string(),
+                        },
+                        start + std::time::Duration::from_millis(2100),
+                    ),
+                    // final tick
+                    dsim::message_bus::SimulatorEvent::Tick(
+                        start + std::time::Duration::from_millis(3000),
+                    ),
+                ]
+            },
+        );
+
+        simulator.run();
+    }
 }
